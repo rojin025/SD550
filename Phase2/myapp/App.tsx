cecbfd925e69e5904a1dd1e3710972a7616a8640
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-05-09 13:24:11 -0500

    Cleaning and organizing my code -style and rendering.

--- Phase2/myapp/App.tsx
+++ Phase2/myapp/App.tsx
@@ -1,19 +1,14 @@
-import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import {
-  Alert,
-  Button,
-  KeyboardAvoidingView,
-  Platform,
-  StyleSheet,
   Text,
-  TextInput,
   Image,
   View,
   SafeAreaView,
   ScrollView,
   FlatList,
 } from "react-native";
+
+import styles from "./styles";
 
 interface ProductI {
   id: number;
@@ -29,10 +24,8 @@ function Product({ product }: props) {
   const { id, name, image } = product;
   return (
     <View>
-      <Text>
-        {id}
-        {name}
-      </Text>
+      <Text>{id}</Text>
+      <Text>{name}</Text>
       <Image source={{ uri: `${image}` }} style={{ height: 100, width: 200 }} />
     </View>
   );
@@ -43,10 +36,10 @@ export default function App() {
 
   useEffect(() => {
     const arr = [];
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 10; i++) {
       arr.push({
         id: i + 1,
-        name: "name " + i,
+        name: "name: " + i,
         image:
           "https://fastly.picsum.photos/id/887/200/300.jpg?hmac=jzro4NZWOPJxqU5bUBoxrD7f4jPtFNB4VmKpn7H5sYI",
       });
@@ -56,33 +49,14 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <SafeAreaView>
-        <ScrollView>
-          <FlatList
-            data={products}
-            renderItem={({ item }) => <Product product={item} />}
-            keyExtractor={(item) => item.id.toString()}
-          />
-        </ScrollView>
-      </SafeAreaView>
+      <ScrollView>
+        <FlatList
+          data={products}
+          renderItem={({ item }) => <Product product={item} />}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </ScrollView>
+      {/* </SafeAreaView> */}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "black",
-    width: 200,
-    fontSize: 20,
-  },
-
-  text: {},
-});
---
+++ Phase2/myapp/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    width: 200,
+    fontSize: 20,
+  },
+
+  text: {},
+});
+
+export default styles;
