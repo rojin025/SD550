be86b30250982a27bcce8c797b59b52c77193061
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-05-10 16:14:55 -0500

    Lecture 6 classwork.

---
+++ Phase2/Lec6/App.tsx
@@ -0,0 +1,34 @@
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./components/Home";
+import Products from "./components/Products";
+
+export default function App() {
+  const { Navigator, Screen } = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Navigator>
+        <Screen
+          name="home"
+          component={Home}
+          options={{ title: "My Home", headerShown: true }}
+        />
+        <Screen name="products" component={Products} />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ Phase2/Lec6/components/Products.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Products() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Products Screen</Text>
+    </View>
+  );
+}
