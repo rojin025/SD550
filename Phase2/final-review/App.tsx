a40f3e294fe03353a575bb884704b076e80f5fdf
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-05-21 15:02:45 -0500

    FinalReview V1.0.1 - working with layout.

--- Phase2/final-review/App.tsx
+++ Phase2/final-review/App.tsx
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.title}>1</Text>
+      </View>
+
+      <View>
+        <Text style={styles.title}>Text</Text>
+      </View>
+      <View>
+        <Text style={styles.title}>2</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +21,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 45,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 30,
+    fontStyle: "italic",
+    fontWeight: 900,
+    textDecorationLine: "underline",
   },
 });
